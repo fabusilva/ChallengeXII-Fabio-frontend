014de9ae604114660ea05a98dffd78959afbbd0f
Fábio Silva <106494125+fabusilva@users.noreply.github.com>
2024-05-13 19:26:43 -0300

    fix:update navbar

--- challenge-xii-frontend/src/components/Navbar/Navbar.tsx
+++ challenge-xii-frontend/src/components/Navbar/Navbar.tsx
@@ -1,52 +1,29 @@
-import { useState } from 'react';
 import Image from 'next/image';
 import style from './Navbar.module.css';
-
+import logo from '../../../public/Logo.png';
+import sino from '../../../public/bell-icon.png';
+import avatar from '../../../public/user-avatar.png';
+import separato from '../../../public/seperator.png';
 export default function Navbar() {
-    const [isPopupVisible, setPopupVisible] = useState(false);
-
-    const togglePopup = () => {
-        setPopupVisible(!isPopupVisible);
-    };
-
-    const closePopup = () => {
-        setPopupVisible(false);
-    };
-
     return (
-        <nav className={style.navbarContainer}>
-            <Image className={style.navbarLogo} src="https://fakeimg.pl/100x100?text=logo" alt="img" width={100} height={100} />
-            
-            <ul className={`${style.navbarItems} ${isPopupVisible ? style.show : ''}`}>
-                <li className={style.navbarText}>Home</li>
-                <li className={style.navbarText}>Getting a Taxi</li>
-                <li className={style.navbarText}>Mobile App</li>
-                <li className={style.navbarText}>Contact Us</li>
-            </ul>
+        <div className={style.navbar}>
+            <nav className={style.navbarContainer}>
+                <Image className={style.navbarLogo} src={logo} alt="img" width={100} height={100} />
+                
+                <ul className={style.navbarItems}>
+                    <li className={style.navbarText}>Home</li>
+                    <li className={style.navbarText}>Getting a Taxi</li>
+                    <li className={style.navbarText}>Mobile App</li>
+                    <li className={style.navbarText}>Contact Us</li>
+                </ul>
 
-            <div className={style.navbarUser}>
-                <Image className ={style.navbarUserIcon} src="https://fakeimg.pl/100x100?text=sininho" alt="img" width={100} height={100} />
-                <p>|</p>
-                <Image className ={style.navbarUserAvatar} src="https://fakeimg.pl/100x100?text=avatar" alt="img" width={100} height={100} />
-            </div>
-            
-            <div className={style.navbarBtResponsivo} onClick={togglePopup}>
-                <Image className ={style.navbarButtonImg} src="https://cdn-icons-png.flaticon.com/128/6063/6063509.png" alt="img" width={100} height={100} />
-            </div>
-
-            {isPopupVisible && (
-                <div className={style.popup}>
-                    <button className={style.closeButton} onClick={closePopup}>
-                        X
-                    </button>
-                    <ul className={style.popupItems}>
-                        <li className={style.popupText}>Home</li>
-                        <li className={style.popupText}>Getting a Taxi</li>
-                        <li className={style.popupText}>Mobile App</li>
-                        <li className={style.popupText}>Contact Us</li>
-                    </ul>
+                <div className={style.navbarUser}>
+                    <Image className ={style.navbarUserIcon} src={sino} alt="img" width={100} />
+                    <Image src={separato} alt="img" />
+                    <Image className ={style.navbarUserAvatar} src={avatar} alt="img" />
                 </div>
-            )}
-        </nav>
+                
+            </nav>
+        </div>
     );
 }
