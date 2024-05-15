59260fc532809ecfb3c04fe37620f0f4cf6e74d9
Fábio Silva <106494125+fabusilva@users.noreply.github.com>
2024-05-15 13:37:11 -0300

    feat:add button component

--- challenge-xii-frontend/src/components/_ui/ButtonSubmit/ButtonSubmit.tsx
+++ challenge-xii-frontend/src/components/_ui/ButtonSubmit/ButtonSubmit.tsx
@@ -1,7 +1,22 @@
-import style from './ButtonSubmit.module.css'
+import Image from 'next/image';
+import style from './ButtonSubmit.module.css';
 
-export default function ButtonSubmit(){
-    return(
-        <button>Button component</button>
-    )
+interface ButtonProps {
+    text: string;
+    url?: string;
+}
+
+export default function ButtonSubmit({ text, url }: ButtonProps) {
+    if (url) {
+        return (
+            <button className={style.button}>
+                <Image src={url} alt="img" />
+                {text}
+            </button>
+        );
+    } else {
+        return (
+            <button className={style.button}>{text}</button>
+        );
+    }
 }
