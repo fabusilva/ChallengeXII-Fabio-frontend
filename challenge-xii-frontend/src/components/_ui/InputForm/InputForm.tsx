a89180368b45a44068c72fb3bedf1bd420d8052c
Fábio Silva <106494125+fabusilva@users.noreply.github.com>
2024-05-15 18:33:05 -0300

    fix:add update input form

--- challenge-xii-frontend/src/components/_ui/InputForm/InputForm.tsx
+++ challenge-xii-frontend/src/components/_ui/InputForm/InputForm.tsx
@@ -3,14 +3,26 @@ import style from './InputForm.module.css'
 interface InputProsp{
     typeInput: string;
     text: string;
-    placeholder?:string;
-    arrayOpcoes?:[string];
+    url?:string;
+    name?: string;
+    validation?: string;
+    value?: string;
+    handleOnChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    placeholder?: string;
 }
-export default function InputForm({typeInput, text,placeholder,arrayOpcoes}:InputProsp){
-    return(
+
+export default function InputForm({ typeInput, text,name,value, placeholder,validation, handleOnChange }: InputProsp) {
+    return (
         <div className={style.inputContainer}>
             <label className={style.labelForm}>{text}</label>
-            <input className={style.inputStyle} type={typeInput} placeholder={placeholder}/>
+            <input
+                className={style.inputStyle}
+                type={typeInput}
+                name={name}
+                value={value}
+                placeholder={placeholder}
+                onChange={handleOnChange}
+            />
         </div>
-    )
+    );
 }
