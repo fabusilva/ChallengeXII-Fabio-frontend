674f8daec90c8e23899c5a2d538af1064879b9b4
Fábio Silva <106494125+fabusilva@users.noreply.github.com>
2024-05-15 17:14:08 -0300

    fix:migrating images to AWS bucket

--- challenge-xii-frontend/src/components/Navbar/Navbar.tsx
+++ challenge-xii-frontend/src/components/Navbar/Navbar.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image';
 import style from './Navbar.module.css';
-import logo from '../../../public/Logo.png';
-import sino from '../../../public/bell-icon.png';
-import avatar from '../../../public/user-avatar.png';
-import separato from '../../../public/seperator.png';
 export default function Navbar() {
+    const logo:string = 'https://challengexiifabio.s3.amazonaws.com/Imagens/Navbar/Logo.png';
+    const sino:string = 'https://challengexiifabio.s3.amazonaws.com/Imagens/Navbar/bell-icon.png';
+    const avatar: string = 'https://challengexiifabio.s3.amazonaws.com/Imagens/Navbar/user-avatar.png';
+    const separato: string = 'https://challengexiifabio.s3.amazonaws.com/Imagens/Navbar/seperator.png';
     return (
         <div className={style.navbar}>
             <nav className={style.navbarContainer}>
-                <Image className={style.navbarLogo} src={logo} alt="img" width={100} height={100} />
+                <Image className={style.navbarLogo} src={logo} alt="img" width={100} height={100}/>
                 
                 <ul className={style.navbarItems}>
                     <li className={style.navbarText}>Home</li>
@@ -18,9 +18,9 @@ export default function Navbar() {
                 </ul>
 
                 <div className={style.navbarUser}>
-                    <Image className ={style.navbarUserIcon} src={sino} alt="img" width={100} />
-                    <Image src={separato} alt="img" />
-                    <Image className ={style.navbarUserAvatar} src={avatar} alt="img" />
+                    <Image className ={style.navbarUserIcon} src={sino} alt="img" width={100} height={100}/>
+                    <Image src={separato} alt="img" width={0.7} height={29.71}/>
+                    <Image className ={style.navbarUserAvatar} src={avatar} alt="img" width={100} height={100}/>
                 </div>
                 
             </nav>
