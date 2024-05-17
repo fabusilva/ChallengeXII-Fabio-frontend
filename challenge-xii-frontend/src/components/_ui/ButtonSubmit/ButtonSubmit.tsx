9ed4fb49f489333f435484e0dde6673b42b640a0
Fábio Silva <106494125+fabusilva@users.noreply.github.com>
2024-05-17 16:04:16 -0300

    feat:add function in button component

--- challenge-xii-frontend/src/components/_ui/ButtonSubmit/ButtonSubmit.tsx
+++ challenge-xii-frontend/src/components/_ui/ButtonSubmit/ButtonSubmit.tsx
@@ -4,12 +4,13 @@ import style from './ButtonSubmit.module.css';
 interface ButtonProps {
     text: string;
     url?: string;
+    onclickProps?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function ButtonSubmit({ text, url }: ButtonProps) {
+export default function ButtonSubmit({ text, url, onclickProps }: ButtonProps) {
     if (url) {
         return (
-            <button className={style.button}>
+            <button onClick={onclickProps} className={style.button}>
                 <Image className={style.imageButton} src={url} alt="img" width={17.49} height={17.49}/>
                 {text}
             </button>
