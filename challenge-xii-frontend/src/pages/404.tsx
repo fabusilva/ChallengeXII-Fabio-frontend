8c6e57a2a601a59617577fbf1d6b15af68e5e4d9
Fábio Silva <106494125+fabusilva@users.noreply.github.com>
2024-05-20 08:34:26 -0300

    fix:update page 404

--- challenge-xii-frontend/src/pages/404.tsx
+++ challenge-xii-frontend/src/pages/404.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
-
-export default function NotFound(){
-    return(
-        <div>
-            <h1>404</h1>
-            <p>Pagina Não disponivel</p>
-            <p>Volta para <Link href="/">Home</Link></p>
-        </div>
-    )
+import style from "../styles/NotFound.module.css";
+export default function NotFound() {
+  return (
+    <div className={style.notFoundContainer}>
+      <div className={style.notFoundCard}>
+      <h1>404</h1>
+        <p>Ops! Página não encontrada.</p>
+        <p>Parece que você tentou acessar uma página que não existe.</p>
+        <p>Vamos ajudar você a encontrar o caminho de volta:</p>
+        <p>
+          Clique <Link href="/">aqui para voltar para Home</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
